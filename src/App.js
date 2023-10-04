95528bed7445061685bb80e56ea855c27b14fe12
Orion551 <gravanteumberto@outlook.com>
2023-10-05 00:04:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+    return (
+        <>
+            <Button variant="contained">Button</Button>
+        </>
+    )
+}
+
+export default App
