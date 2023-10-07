72b7db50c73359d07314dd2c75ca0a14e9ca30a3
Orion551 <gravanteumberto@outlook.com>
2023-10-07 12:47:45 +0200

    Introduced (static) page-title component and applied some css

--- src/components/page-title.js
+++ src/components/page-title.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import '../assets/styles/page-title.css';
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
 
+
+import IconButton from '@mui/material/IconButton'
 import CalendarViewWeekIcon from '@mui/icons-material/CalendarViewWeek';
 // import FolderIcon from '@mui/icons-material/Folder';
 // import StyleIcon from '@mui/icons-material/Style';
 // import AnalyticsIcon from '@mui/icons-material/Analytics';
+
+
+export const CustomIcon = styled(CalendarViewWeekIcon)`
+  width: 45px !important;
+  height: 45px !important;
+  color: #1E1E1E !important;
+`;
 
 export default function PageTitle() {
     return (
@@ -15,9 +26,17 @@ export default function PageTitle() {
               * For now, will be Schedule (since it's current feature I'm working on)
             */}
             <div className="title-container">
-                <CalendarViewWeekIcon />
+                <IconButton>
+                    <CustomIcon />
+                </IconButton>
+                {/*<CalendarViewWeekIcon>*/}
+                {/*    */}
+                {/*</ CalendarViewWeekIcon>*/}
+
                 {/* TODO: This will be dynamic */}
-                <span>Schedule</span>
+                <Typography variant="h5" className="text-test">
+                    Schedule
+                </Typography>
             </div>
         </>
     )
