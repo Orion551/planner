4de3cedae589269e904e5c3d8c459592b3bd55cc
Orion551 <gravanteumberto@outlook.com>
2023-10-24 23:56:15 +0200

    implemented dynamic icon based on current view

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,39 +1,28 @@
 import * as React from 'react';
 import '../assets/styles/page-title.css';
-import styled from 'styled-components';
 import { Typography } from '@mui/material';
-
-
-import IconButton from '@mui/material/IconButton'
 import CalendarViewWeekIcon from '@mui/icons-material/CalendarViewWeek';
-// import FolderIcon from '@mui/icons-material/Folder';
-// import StyleIcon from '@mui/icons-material/Style';
-// import AnalyticsIcon from '@mui/icons-material/Analytics';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import FolderIcon from '@mui/icons-material/Folder';
+import StyleIcon from '@mui/icons-material/Style';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
 
+const icons = {
+    'Schedule': <CalendarViewWeekIcon />,
+    'Tags': <StyleIcon />,
+    'Analytics': <AnalyticsIcon />,
+    'Projects': <FolderIcon />,
+};
 
-export const CustomIcon = styled(CalendarViewWeekIcon)`
-  width: 45px !important;
-  height: 45px !important;
-  color: #1E1E1E !important;
-`;
 
 export default function PageTitle({currentView}) {
+    const routeIcon = icons[currentView] || <EmojiEmotionsIcon />;
+
     return (
         <>
-            {/*
-              * This should get its value from navbar buttons
-              * which represent app's current state.
-              * For now, will be Schedule (since it's current feature I'm working on)
-            */}
             <div className="title-container">
-                <IconButton>
-                    <CustomIcon />
-                </IconButton>
-                {/*<CalendarViewWeekIcon>*/}
-                {/*    */}
-                {/*</ CalendarViewWeekIcon>*/}
+                <div className="page-title-icon">{routeIcon}</div>
 
-                {/* TODO: This will be dynamic */}
                 <Typography variant="h5" className="text-test">
                     {currentView}
                 </Typography>
