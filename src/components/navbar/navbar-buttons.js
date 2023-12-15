bda56520b027a75369fe2fa1c0bacd48ed203d98
Orion551 <gravanteumberto@outlook.com>
2023-12-15 15:25:10 +0100

    Navbar buttons should be selected when a section is selected

--- src/components/navbar/navbar-buttons.js
+++ src/components/navbar/navbar-buttons.js
@@ -1,33 +1,41 @@
 import * as React from 'react';
 import { Typography } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Button } from "@mui/material";
 
 import CalendarViewWeekIcon from '@mui/icons-material/CalendarViewWeek';
 import FolderIcon from '@mui/icons-material/Folder';
-import StyleIcon from '@mui/icons-material/Style';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
 
 export default function NavbarButtons() {
     return (
         <>
-            <Link to="/">
+            <NavLink
+                to="/"
+                className={({isActive}) => isActive ? "active" : ""}
+            >
                 <Button className="navbar-button" startIcon={<CalendarViewWeekIcon />}>
                     <Typography variant="overline">Schedule</Typography>
                 </Button>
-            </Link>
+            </NavLink>
 
-            <Link to="projects">
+            <NavLink
+                to="projects"
+                className={({isActive}) => isActive ? "active" : ""}
+            >
                 <Button className="navbar-button" startIcon={<FolderIcon />}>
                     <Typography variant="overline">Projects</Typography>
                 </Button>
-            </Link>
+            </NavLink>
 
-            <Link to="analytics">
+            <NavLink
+                to="analytics"
+                className={({isActive}) => isActive ? "active" : ""}
+            >
                 <Button className="navbar-button" startIcon={<AnalyticsIcon />}>
                     <Typography variant="overline">Analytics</Typography>
                 </Button>
-            </Link>
+            </NavLink>
         </>
     );
 }
