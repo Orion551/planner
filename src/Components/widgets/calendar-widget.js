0031d501437d5113dde15559cedb77dde105d0ae
Orion551 <gravanteumberto@outlook.com>
2024-03-11 20:28:35 +0100

    Refactored calendar-widget to functional component and translated its name

--- src/Components/widgets/calendar-widget.js
+++ src/Components/widgets/calendar-widget.js
@@ -3,39 +3,38 @@ import '@Assets/styles/widget.scss';
 import { Typography } from '@mui/material';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import Grid from '@mui/material/Grid';
+import { useTranslation } from 'react-i18next';
 
-export class CalendarWidget extends React.Component {
-  widgetName = this.props.widgetName;
-  render() {
-    return (
-      <>
-        <div id={'widget-container'} className={`${this.widgetName}`}>
-          <Grid
-            container
-            direction='column'
-            justifyContent='flex-start'
-            alignItems='flex-start'
-            spacing={0.5}
-          >
-            <Grid item xs>
-              <Grid container direction='row' className='widget-header' spacing={0.5}>
-                <CalendarMonthIcon />
-                <Typography className='widget-header-title' variant='h6'>
-                  Calendar
-                </Typography>
-              </Grid>
-            </Grid>
-
-            <Grid item xs>
-              <Typography variant='h6'>December, 2023</Typography>
-            </Grid>
-
-            <Grid item xs>
-              <Typography variant='h6'>Week 3, 18 - 24</Typography>
+export const CalendarWidget = ({ widgetName }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div id={'widget-container'} className={`${widgetName}`}>
+        <Grid
+          container
+          direction='column'
+          justifyContent='flex-start'
+          alignItems='flex-start'
+          spacing={0.5}
+        >
+          <Grid item xs>
+            <Grid container direction='row' className='widget-header' spacing={0.5}>
+              <CalendarMonthIcon />
+              <Typography className='widget-header-title' variant='h6'>
+                {t('widgets.title.calendar')}
+              </Typography>
             </Grid>
           </Grid>
-        </div>
-      </>
-    );
-  }
-}
+
+          <Grid item xs>
+            <Typography variant='h6'>December, 2023</Typography>
+          </Grid>
+
+          <Grid item xs>
+            <Typography variant='h6'>Week 3, 18 - 24</Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
