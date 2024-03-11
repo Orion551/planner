e410b809edb00fc34290363dd6982c73ad06ae32
Orion551 <gravanteumberto@outlook.com>
2024-03-11 20:33:48 +0100

    Translated completed-activities widget

--- src/Components/widgets/completed-activities-widget.js
+++ src/Components/widgets/completed-activities-widget.js
@@ -3,36 +3,34 @@ import '@Assets/styles/widget.scss';
 import DoneIcon from '@mui/icons-material/Done';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
-export class CompletedActivitiesWidget extends React.Component {
-  widgetName = this.props.widgetName;
-  completedActivities = this.props.compltedActivities;
-  render() {
-    return (
-      <>
-        <div id={'widget-container'} className={`${this.widgetName}`}>
-          <Grid
-            container
-            direction='column'
-            justifyContent='flex-start'
-            alignItems='flex-start'
-            spacing={0.5}
-          >
-            <Grid item xs>
-              <Grid container direction='row' className='widget-header' spacing={0.5}>
-                <DoneIcon />
-                <Typography className='widget-header-title' variant='h6'>
-                  Completed this week
-                </Typography>
-              </Grid>
-            </Grid>
-
-            <Grid item xs>
-              <Typography variant='h3'>{this.completedActivities}</Typography>
+export const CompletedActivitiesWidget = ({ widgetName, compltedActivities }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div id={'widget-container'} className={`${widgetName}`}>
+        <Grid
+          container
+          direction='column'
+          justifyContent='flex-start'
+          alignItems='flex-start'
+          spacing={0.5}
+        >
+          <Grid item xs>
+            <Grid container direction='row' className='widget-header' spacing={0.5}>
+              <DoneIcon />
+              <Typography className='widget-header-title' variant='h6'>
+                {t('widgets.title.completed')}
+              </Typography>
             </Grid>
           </Grid>
-        </div>
-      </>
-    );
-  }
-}
+
+          <Grid item xs>
+            <Typography variant='h3'>{compltedActivities}</Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
