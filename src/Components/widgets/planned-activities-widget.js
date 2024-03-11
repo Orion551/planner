52425466de2adb3346aaf0be2dd2753abcd9e5eb
Orion551 <gravanteumberto@outlook.com>
2024-03-11 20:36:29 +0100

    Refactored planned-activities-widget to be a functional component as well and added relative translations

--- src/Components/widgets/planned-activities-widget.js
+++ src/Components/widgets/planned-activities-widget.js
@@ -3,37 +3,34 @@ import '@Assets/styles/widget.scss';
 import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import InsertChartIcon from '@mui/icons-material/InsertChart';
+import { useTranslation } from 'react-i18next';
 
-export class PlannedActivitiesWidget extends React.Component {
-  widgetName = this.props.widgetName;
-  plannedActivities = this.props.plannedActivities;
-
-  render() {
-    return (
-      <>
-        <div id={'widget-container'} className={`${this.widgetName}`}>
-          <Grid
-            container
-            direction='column'
-            justifyContent='flex-start'
-            alignItems='flex-start'
-            spacing={0.5}
-          >
-            <Grid item xs>
-              <Grid container direction='row' className='widget-header' spacing={0.5}>
-                <InsertChartIcon />
-                <Typography className='widget-header-title' variant='h6'>
-                  Planned Activities
-                </Typography>
-              </Grid>
-            </Grid>
-
-            <Grid item xs>
-              <Typography variant='h3'>{this.plannedActivities}</Typography>
+export const PlannedActivitiesWidget = ({ widgetName, plannedActivities }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div id={'widget-container'} className={`${widgetName}`}>
+        <Grid
+          container
+          direction='column'
+          justifyContent='flex-start'
+          alignItems='flex-start'
+          spacing={0.5}
+        >
+          <Grid item xs>
+            <Grid container direction='row' className='widget-header' spacing={0.5}>
+              <InsertChartIcon />
+              <Typography className='widget-header-title' variant='h6'>
+                {t('widgets.title.planned_activities')}
+              </Typography>
             </Grid>
           </Grid>
-        </div>
-      </>
-    );
-  }
-}
+
+          <Grid item xs>
+            <Typography variant='h3'>{plannedActivities}</Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
