d4b5919751d7b6e4d2af96c0ca11ca401ed2f063
Orion551 <gravanteumberto@outlook.com>
2024-03-11 20:22:47 +0100

    Added browser language auto-detect feature and brought localization files outside

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -2,30 +2,29 @@
 import i18n from 'i18next';
 // Bindings for React: allow components to re-render when language changes
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslation from '@Assets/locales/en.json';
+import itTranslation from '@Assets/locales/it.json';
+
+export const supportedLangs = {
+  en: 'English',
+  it: 'Italian',
+};
 
 export default i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // Add React bindings as a plugin
   .init({
-    // Config opts
-    // Default lang
-    lng: 'en',
-    // Fallback lang
     fallbackLng: 'en',
+    detection: {
+      order: ['navigator'],
+    },
+    resources: {
+      en: { translation: enTranslation },
+      it: { translation: itTranslation },
+    },
     debug: true,
     interpolation: {
       escapeValue: false,
-    },
-    resources: {
-      // English
-      en: {
-        translation: {
-          calendar: 'Calendar',
-        },
-      },
-      it: {
-        translation: {
-          calendar: 'Calendario',
-        },
-      },
     },
   });
