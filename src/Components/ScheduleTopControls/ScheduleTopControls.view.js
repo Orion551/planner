e95b9cf5584730e53ecb88437f663bad162e5f3b
Orion551 <gravanteumberto@outlook.com>
2024-03-14 08:34:19 +0100

    **unstable** Added some translations and implemented first lines of code

--- src/Components/ScheduleTopControls/ScheduleTopControls.view.js
+++ src/Components/ScheduleTopControls/ScheduleTopControls.view.js
@@ -6,6 +6,7 @@ export const ScheduleTopControlsView = () => {
     <>
       <div>
         <TagsButtonView />
+        {/* <create-activity-button /> */}
       </div>
     </>
   );
--- src/Components/Tags/TagsMenu.view.js
+++ src/Components/Tags/TagsMenu.view.js
@@ -1,13 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getRequest } from '@Api/http-service';
 import { ApiUrl } from '@Utils/ApiUrl';
+import { useTranslation } from 'react-i18next';
 // import { AppBar, Toolbar, List, ListItem, IconButton, Input, Button } from '@mui/material';
 
 export const TagsMenuView = () => {
-  //   const [isSubMenuOpen, setSubMenuOpen] = useState(false);
-  //   const [searchQuery, setSearchQuery] = useState('');
-  //   const [selectedItem, setSelectedItem] = useState(null);
-  //   const [selectedColor, setSelectedColor] = useState(null);
+  const { t } = useTranslation();
+
+  // const [isSubMenuOpen, setSubMenuOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  // const [selectedItem, setSelectedItem] = useState(null);
+  // const [selectedColor, setSelectedColor] = useState(null);
+
+  // const handleItemClick = (item) => {
+  //   setSelectedItem(item);
+  //   setSubMenuOpen(true);
+  // };
+
+  // const handleColorButtonClick = (color) => {
+  //   setSelectedColor(color);
+  // };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    // Perform search logic here
+  };
+
   useEffect(() => {
     (async function () {
       try {
@@ -22,8 +40,38 @@ export const TagsMenuView = () => {
 
   return (
     <>
-      {/* TODO: Iterate through TagElement(s) and add some controls over them */}
-      <span>This will be the tags menu :)</span>
+      <div>
+        <input
+          type='text'
+          placeholder={t('schedule_top_controls.tags.search_or_create_a_tag')}
+          value={searchQuery}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+
+        {/* <List>
+          {items.map((item) => (
+            <ListItem key={item.id}>
+              {item.text}
+              <IconButton onClick={() => handleItemClick(item)}>Icon</IconButton>
+            </ListItem>
+          ))}
+        </List>
+        {isSubMenuOpen && (
+          <div>
+            <Input placeholder='Submenu Input' />
+            <Button>Delete</Button>
+            <div>
+              {colors.map((color) => (
+                <Button
+                  key={color}
+                  style={{ backgroundColor: color }}
+                  onClick={() => handleColorButtonClick(color)}
+                />
+              ))}
+            </div>
+          </div>
+        )} */}
+      </div>
     </>
   );
 };
