2577287f261e6f883ea10aa484bcef01e1b7ac2e
Orion551 <gravanteumberto@outlook.com>
2024-03-16 12:09:42 +0100

    updated rest service with correct url

--- src/Api/planner-rest-service.js
+++ src/Api/planner-rest-service.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
  * At the moment this will head to some mock resources provided by Mockoon
  */
 export const PlannerRestService = Axios.create({
-  baseURL: 'https://6eb75b23-7830-436d-92c8-fdf7bbb533e4.mock.pstmn.io',
+  baseURL: 'https://e7517467-30d8-4b58-a190-d3d365fe981b.mock.pstmn.io',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
