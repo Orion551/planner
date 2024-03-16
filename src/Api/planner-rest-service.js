f669e2ae0a94c01c6ddc2b60962cd45ca1548145
Orion551 <gravanteumberto@outlook.com>
2024-03-16 13:12:14 +0100

    Reverted rest service to head to local instance

--- src/Api/planner-rest-service.js
+++ src/Api/planner-rest-service.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
  * At the moment this will head to some mock resources provided by Mockoon
  */
 export const PlannerRestService = Axios.create({
-  baseURL: 'https://e7517467-30d8-4b58-a190-d3d365fe981b.mock.pstmn.io',
+  baseURL: 'http://localhost:3000',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
