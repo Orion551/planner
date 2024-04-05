210df786e2b7f136539c183884c89ec5b0d4a9ea
Orion551 <gravanteumberto@outlook.com>
2024-04-05 10:41:04 +0200

    Added some comment to utils function

--- src/Utils/GetCurrentDayName.js
+++ src/Utils/GetCurrentDayName.js
@@ -1,4 +1,7 @@
 import { DaysOfWeek } from '@Constants/DaysOfWeek';
+/**
+ * @returns {string} Uses Date() method to retrieve current day, then returns the long-version of the day (e.g Saturday instead of Sat)
+ */
 export const getCurrentDayName = () => {
   return DaysOfWeek[new Date().getDay()];
 };
