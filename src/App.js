38ec34fe1ab53f28ec64edd7a7ba7cf106f7a54e
Orion551 <gravanteumberto@outlook.com>
2024-04-27 12:08:57 +0200

    implemented top-app-bar and hidden drawer on large screens. Trying to simplify code structure

--- src/App.js
+++ src/App.js
@@ -1,67 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Drawer from '@mui/material/Drawer';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavbarView } from '@Components/Navbar/Navbar.view';
-import { Schedule } from '@Pages/Schedule';
-import { Projects } from '@Pages/Projects';
-import { Analytics } from '@Pages/Analytics';
-import { ErrorPage } from '@Pages/ErrorPage';
+// import { Schedule } from '@Pages/Schedule';
+// import { Projects } from '@Pages/Projects';
+// import { Analytics } from '@Pages/Analytics';
+// import { ErrorPage } from '@Pages/ErrorPage';
 import './Assets/styles/global.scss';
-import Grid from '@mui/material/Grid';
-import { useGlobalState } from '@Context/GlobalStateContext';
-import { getRequest } from '@Api/http-service';
-import CircularProgress from '@mui/material/CircularProgress';
-import { initConfig } from '@Context/GlobalStateContext';
-import { ApiUrl } from '@Constants/ApiUrl';
+// import { useGlobalState } from '@Context/GlobalStateContext';
+// import { getRequest } from '@Api/http-service';
+// import CircularProgress from '@mui/material/CircularProgress';
+// import { initConfig } from '@Context/GlobalStateContext';
+// import { ApiUrl } from '@Constants/ApiUrl';
+import { Box, IconButton, Typography } from '@mui/material';
+
+import AppBar from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
 
 export function App() {
-  const { state: appState, dispatch } = useGlobalState();
-  const [isLoading, setIsLoading] = useState(true);
+  const drawerWidth = 240;
+  // const { state: appState, dispatch } = useGlobalState();
+  // const [isLoading, setIsLoading] = useState(true);
+  const [mobileOpen, setMobileOpen] = useState(false);
+  mobileOpen;
 
-  useEffect(() => {
-    (async function () {
-      try {
-        const response = await getRequest({ url: ApiUrl.plannerConfig });
-        dispatch(initConfig(response));
-        setIsLoading(false);
-      } catch (e) {
-        console.error(e.message);
-        setIsLoading(false);
-      }
-    })();
-  }, [dispatch]);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  // useEffect(() => {
+  //   (async function () {
+  //     try {
+  //       const response = await getRequest({ url: ApiUrl.plannerConfig });
+  //       dispatch(initConfig(response));
+  //       setIsLoading(false);
+  //     } catch (e) {
+  //       console.error(e.message);
+  //       setIsLoading(false);
+  //     }
+  //   })();
+  // }, [dispatch]);
 
   return (
-    <Router>
-      <Grid
-        container
-        direction='row'
-        justifyContent='flex-start'
-        alignItems='flex-start'
-        spacing={1}
-      >
-        <Grid item xs={2}>
-          <NavbarView />
-        </Grid>
-        <Grid item xs={10} p={1}>
-          {isLoading ? ( // Conditional rendering based on loading state
-            <CircularProgress /> // Render loading indicator or placeholder
-          ) : (
-            <Routes>
-              {appState.configData ? ( // Check if configData is available
-                <>
-                  <Route path='/' element={<Schedule />} index />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/analytics' element={<Analytics />} />
-                  <Route path='*' element={<ErrorPage />} />
-                </>
-              ) : (
-                /* TODO: of course improve that */
-                <span>no stuff</span>
-              )}
-            </Routes>
-          )}
-        </Grid>
-      </Grid>
-    </Router>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component='nav'>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            edge='start'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            PLANNER
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          <NavbarView handleDrawerToggle={handleDrawerToggle} />
+        </Drawer>
+      </nav>
+      <Box component='main' sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>Actual content</Typography>
+      </Box>
+    </Box>
   );
 }
--- src/Components/Navbar/Navbar.view.js
+++ src/Components/Navbar/Navbar.view.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import logo from '@Assets/images/logo_tmp.png';
-import { NavbarButtonsView } from '@Components/Navbar/NavbarButtons.view';
-import { Outlet } from 'react-router-dom';
-import { Stack } from '@mui/material';
+// import logo from '@Assets/images/logo_tmp.png';
+// import { NavbarButtonsView } from '@Components/Navbar/NavbarButtons.view';
+// import { Outlet } from 'react-router-dom';
 import '@Assets/styles/navbar.scss';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+// import { Link } from 'react-router-dom';
 
-export function NavbarView() {
+export function NavbarView({ handleDrawerToggle }) {
   return (
-    <>
-      <div className='navbar'>
-        <Stack
-          direction='column'
-          justifyContent='flex-start'
-          alignItems='center'
-          flexWrap='nowrap'
-          spacing={2}
-        >
-          <img src={logo} alt='Logo' className='logo' />
-          <NavbarButtonsView />
-        </Stack>
-        <div>
-          <Outlet />
-        </div>
-      </div>
-    </>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        Test
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem to='/'>
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary='Schedule' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem to='/projects'>
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary='Projects' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem to='/analytics'>
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary='Analytics' />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
   );
 }
