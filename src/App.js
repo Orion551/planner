14d30de92cd3446439bf74adde04390ad9d1c298
Orion551 <gravanteumberto@outlook.com>
2024-04-27 15:32:36 +0200

    Restored views in Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-// import React, { useEffect, useState } from 'react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Drawer from '@mui/material/Drawer';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { NavbarView } from '@Components/Navbar/Navbar.view';
-// import { Schedule } from '@Pages/Schedule';
-// import { Projects } from '@Pages/Projects';
-// import { Analytics } from '@Pages/Analytics';
-// import { ErrorPage } from '@Pages/ErrorPage';
+import { Schedule } from '@Pages/Schedule';
+import { Projects } from '@Pages/Projects';
+import { Analytics } from '@Pages/Analytics';
+import { ErrorPage } from '@Pages/ErrorPage';
 import './Assets/styles/global.scss';
-// import { useGlobalState } from '@Context/GlobalStateContext';
-// import { getRequest } from '@Api/http-service';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import { initConfig } from '@Context/GlobalStateContext';
-// import { ApiUrl } from '@Constants/ApiUrl';
+import { useGlobalState } from '@Context/GlobalStateContext';
+import { getRequest } from '@Api/http-service';
+import CircularProgress from '@mui/material/CircularProgress';
+import { initConfig } from '@Context/GlobalStateContext';
+import { ApiUrl } from '@Constants/ApiUrl';
 import { Box, IconButton, Typography } from '@mui/material';
 
 import AppBar from '@mui/material/AppBar';
@@ -23,8 +21,8 @@ import Toolbar from '@mui/material/Toolbar';
 
 export function App() {
   const drawerWidth = 240;
-  // const { state: appState, dispatch } = useGlobalState();
-  // const [isLoading, setIsLoading] = useState(true);
+  const { state: appState, dispatch } = useGlobalState();
+  const [isLoading, setIsLoading] = useState(true);
   const [mobileOpen, setMobileOpen] = useState(false);
   mobileOpen;
 
@@ -32,18 +30,18 @@ export function App() {
     setMobileOpen((prevState) => !prevState);
   };
 
-  // useEffect(() => {
-  //   (async function () {
-  //     try {
-  //       const response = await getRequest({ url: ApiUrl.plannerConfig });
-  //       dispatch(initConfig(response));
-  //       setIsLoading(false);
-  //     } catch (e) {
-  //       console.error(e.message);
-  //       setIsLoading(false);
-  //     }
-  //   })();
-  // }, [dispatch]);
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await getRequest({ url: ApiUrl.plannerConfig });
+        dispatch(initConfig(response));
+        setIsLoading(false);
+      } catch (e) {
+        console.error(e.message);
+        setIsLoading(false);
+      }
+    })();
+  }, [dispatch]);
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -88,7 +86,23 @@ export function App() {
         </nav>
         <Box component='main' sx={{ p: 3 }}>
           <Toolbar />
-          <Typography>Actual content</Typography>
+          {isLoading ? ( // Conditional rendering based on loading state
+            <CircularProgress /> // Render loading indicator or placeholder
+          ) : (
+            <Routes>
+              {appState.configData ? ( // Check if configData is available
+                <>
+                  <Route path='/' element={<Schedule />} index />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/analytics' element={<Analytics />} />
+                  <Route path='*' element={<ErrorPage />} />
+                </>
+              ) : (
+                /* TODO: of course improve that */
+                <span>no stuff</span>
+              )}
+            </Routes>
+          )}
         </Box>
       </Router>
     </Box>
