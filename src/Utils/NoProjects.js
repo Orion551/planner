ae8d1ec46f2855e12c2a77804d5a52fc92d3a573
Orion551 <gravanteumberto@outlook.com>
2024-05-04 00:22:03 +0200

    Added 'Create one' label + added some stylization

---
+++ src/Utils/NoProjects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
+
+export const NoProjects = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Box display='flex' flexDirection='column' sx={{ textAlign: 'center', marginBottom: '15px' }}>
+        <Typography variant='h4'>{t('projects.it_looks_like_there_are_no_projects')}</Typography>
+        <Typography variant='h4'>{t('projects.create_one')}</Typography>
+      </Box>
+    </>
+  );
+};
