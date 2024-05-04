c2e3346bf291b84c9022fa8de5d8cdeffd9d8fb5
Orion551 <gravanteumberto@outlook.com>
2024-05-04 09:48:28 +0200

    Projects modal translations

--- src/Components/ProjectsModal/ProjectsModal.view.js
+++ src/Components/ProjectsModal/ProjectsModal.view.js
@@ -3,14 +3,21 @@ import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/
 import IconButton from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useTranslation } from 'react-i18next';
+import { useGlobalState } from '@Context/GlobalStateContext';
 
 export const ProjectsModalView = () => {
+  const { state: appState } = useGlobalState();
   const { t } = useTranslation();
 
   return (
     <React.Fragment>
-      <Dialog scroll='paper' PaperProps={{ component: 'form' }} fullWidth={true}>
-        <DialogTitle>New Project</DialogTitle>
+      <Dialog
+        open={appState.projectsModal.isProjectsModalOpen}
+        scroll='paper'
+        PaperProps={{ component: 'form' }}
+        fullWidth={true}
+      >
+        <DialogTitle>{t('projects_modal.new_project')}</DialogTitle>
         <IconButton
           sx={{ position: 'absolute', right: 8, top: 8, color: (theme) => theme.palette.grey[500] }}
         >
