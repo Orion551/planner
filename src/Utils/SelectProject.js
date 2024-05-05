e8b4789a86628dadb9e5796d4a724397bb7219d1
Orion551 <gravanteumberto@outlook.com>
2024-05-05 21:41:36 +0200

    Added translations for previous created label

--- src/Utils/SelectProject.js
+++ src/Utils/SelectProject.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { Typography } from '@mui/material';
 import { Box } from '@mui/material';
 
 export const SelectProject = () => {
-  //   const { t } = useTranslation();
+  const { t } = useTranslation();
   return (
     <>
       <Box
@@ -12,7 +12,9 @@ export const SelectProject = () => {
         flexDirection='column'
         sx={{ textAlign: 'center', marginBottom: '15px', color: 'text.hint' }}
       >
-        <Typography variant='h4'>Select a Project on the left to get started!</Typography>
+        <Typography variant='h4'>
+          {t('projects.select_a_project_on_the_left_to_get_started')}
+        </Typography>
       </Box>
     </>
   );
