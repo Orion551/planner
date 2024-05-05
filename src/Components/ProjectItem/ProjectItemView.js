db00b6c1547f2494b933c469425661fcb27e4be7
Orion551 <gravanteumberto@outlook.com>
2024-05-05 21:15:05 +0200

    Improved mock projectitem template

--- src/Components/ProjectItem/ProjectItemView.js
+++ src/Components/ProjectItem/ProjectItemView.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { ListItem, ListItemText, ListItemAvatar, Avatar, IconButton } from '@mui/material';
-import Delete from '@mui/icons-material/Delete';
-import Folder from '@mui/icons-material/Folder';
+// import { ListItem, ListItemText, ListItemAvatar, Avatar, IconButton } from '@mui/material';
+// import Delete from '@mui/icons-material/Delete';
+// import Folder from '@mui/icons-material/Folder';
+import { Box, Typography } from '@mui/material';
 
 export const ProjectItemView = ({ project, isSelected }) => {
   isSelected;
   project;
   return (
     <>
-      <ListItem
-        secondaryAction={
-          <IconButton edge='end' aria-label='delete'>
-            <Delete />
-          </IconButton>
-        }
+      <Box
+        display='flex'
+        flexDirection='row'
+        sx={{
+          border: '1px solid black',
+          borderRadius: '10px',
+          width: '250px',
+          height: '40px',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          cursor: 'pointer',
+          marginBottom: '12px',
+        }}
       >
-        <ListItemAvatar>
-          <Avatar>
-            <Folder />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={project.projectName} />
-      </ListItem>
+        <span>O</span>
+        <span>
+          <Typography variant='body1' sx={{ textTransform: 'none' }}>
+            {project.projectName}
+          </Typography>
+        </span>
+        <span>V</span>
+      </Box>
     </>
   );
 };
