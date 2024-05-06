301c099c366df7da8c8d6565dc638a8b9a61a67c
Orion551 <gravanteumberto@outlook.com>
2024-05-06 19:50:53 +0200

    Removed recharts dependency

--- src/Components/ProjectInfo/ProjectSummaryView.js
+++ src/Components/ProjectInfo/ProjectSummaryView.js
@@ -4,19 +4,18 @@ import { Typography, TextField } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { AttachmentWidget } from '@Components/widgets/attachment-widget';
 import Stack from '@mui/material/Stack';
-// import { PieChart, Pie, Tooltip } from 'recharts';
 
 export const ProjectSummaryView = ({ summaryData }) => {
   const { t } = useTranslation();
 
-  // const data02 = [
-  //   { name: 'Group A', value: 2400 },
-  //   { name: 'Group B', value: 4567 },
-  //   { name: 'Group C', value: 1398 },
-  //   { name: 'Group D', value: 9800 },
-  //   { name: 'Group E', value: 3908 },
-  //   { name: 'Group F', value: 4800 },
-  // ];
+  const data02 = [
+    { name: 'Group A', value: 2400 },
+    { name: 'Group B', value: 4567 },
+    { name: 'Group C', value: 1398 },
+    { name: 'Group D', value: 9800 },
+    { name: 'Group E', value: 3908 },
+    { name: 'Group F', value: 4800 },
+  ];
 
   return (
     <>
@@ -65,21 +64,10 @@ export const ProjectSummaryView = ({ summaryData }) => {
         )}
       </Box>
 
-      <Box>
-        {/* <ResponsiveContainer width='100%' height='100%'> */}
-        {/* <PieChart width={400} height={400}>
-          <Pie
-            dataKey='value'
-            data={data02}
-            cx={500}
-            cy={200}
-            innerRadius={40}
-            outerRadius={80}
-            fill='#82ca9d'
-          />
-          <Tooltip />
-        </PieChart> */}
-        {/* </ResponsiveContainer> */}
+      <Box display='flex' flexDirection='column' marginTop={2}>
+        <Typography variant='body1' sx={{ fontWeight: 500, color: 'text.primary' }}>
+          {t('projects.fields.summaryField.summary')}
+        </Typography>
       </Box>
     </>
   );
