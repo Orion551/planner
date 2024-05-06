9404e6a1fcf419463f420b829fb430c01f5cd325
Orion551 <gravanteumberto@outlook.com>
2024-05-06 17:08:54 +0200

    Added new translations; Created attachments field

--- src/Components/ProjectInfo/ProjectSummaryView.js
+++ src/Components/ProjectInfo/ProjectSummaryView.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { Typography, TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 export const ProjectSummaryView = ({ summaryData }) => {
+  const { t } = useTranslation();
   return (
     <>
       <Box display='flex' flexDirection='row'>
         <Box marginRight={3}>
-          <Typography variant='body1'>Tags</Typography>
+          <Typography variant='body1' sx={{ fontWeight: 500, color: 'text.primary' }}>
+            {t('projects.fields.tags')}
+          </Typography>
         </Box>
         <Box>{summaryData.projectTags.map((tag) => tag)}</Box>
       </Box>
@@ -16,12 +20,28 @@ export const ProjectSummaryView = ({ summaryData }) => {
         <TextField
           id='project-description'
           fullWidth
-          label='Description'
+          label={t('projects.fields.descriptionField.description')}
           multiline
           rows={4}
-          placeholder={summaryData.projectDescription === '' ? 'What s that project about?' : ''}
+          placeholder={
+            summaryData.projectDescription === ''
+              ? t('projects.fields.descriptionField.whats_that_project_about')
+              : ''
+          }
           value={summaryData.projectDescription !== '' ? summaryData.projectDescription : ''}
         />
+      </Box>
+
+      <Box display='flex' flexDirection='column' marginTop={2}>
+        <Typography variant='body1' sx={{ fontWeight: 500, color: 'text.primary' }}>
+          {t('projects.fields.attachmentsField.attachments')} (
+          {summaryData.projectAttachments.length})
+        </Typography>
+        <Box marginTop={1} textAlign='center'>
+          <Typography variant='body2' sx={{ color: 'text.hint' }}>
+            {t('projects.fields.attachmentsField.no_attachments')}
+          </Typography>
+        </Box>
       </Box>
     </>
   );
