4cb0f5c5a1a0da1703e910670af69bceed8a601a
Orion551 <gravanteumberto@outlook.com>
2024-05-06 14:06:01 +0200

    Created ProjectInfoView subsections

--- src/Components/ProjectInfo/ProjectInfoView.js
+++ src/Components/ProjectInfo/ProjectInfoView.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import { Typography } from '@mui/material';
 
-export const ProjectInfoView = () => {
-  return <>Hello</>;
+export const ProjectInfoView = ({ project }) => {
+  return (
+    <>
+      <Box
+        display='flex'
+        flexDirection='column'
+        alignItems='flex-start'
+        padding={1}
+        component='subsection'
+      >
+        <Typography variant='h5'>{project.projectName}</Typography>
+        {/* {project.projectId} */}
+      </Box>
+    </>
+  );
 };
