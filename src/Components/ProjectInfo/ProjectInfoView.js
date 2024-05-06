12f30b74e9570bf2641ec80d970298176bd3d427
Orion551 <gravanteumberto@outlook.com>
2024-05-06 15:28:00 +0200

    Added ToggleButton to handle viewChange

--- src/Components/ProjectInfo/ProjectInfoView.js
+++ src/Components/ProjectInfo/ProjectInfoView.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 import { Typography } from '@mui/material';
-import Switch from '@mui/material/Switch';
-import Stack from '@mui/material/Stack';
+import { ToggleButtonGroup, ToggleButton } from '@mui/material';
 
 export const ProjectInfoView = ({ project }) => {
+  const [view, setView] = useState('summary');
+  view;
+
+  const handleViewChange = (event, newView) => {
+    console.log('new view', newView);
+    if (newView !== null) {
+      setView(newView);
+    }
+  };
   return (
     <>
       <Box
@@ -12,9 +20,25 @@ export const ProjectInfoView = ({ project }) => {
         flexDirection='column'
         alignItems='flex-start'
         padding={1}
-        component='subsection'
+        component='section'
       >
         <Typography variant='h5'>{project.projectName}</Typography>
+
+        <ToggleButtonGroup
+          color='primary'
+          exclusive
+          value={view}
+          onChange={handleViewChange}
+          size='small'
+        >
+          <ToggleButton value='summary'>
+            <Typography variant='button'>Summary</Typography>
+          </ToggleButton>
+          ,
+          <ToggleButton value='activities'>
+            <Typography variant='button'>Activities</Typography>
+          </ToggleButton>
+        </ToggleButtonGroup>
         {/* {project.projectId} */}
       </Box>
     </>
