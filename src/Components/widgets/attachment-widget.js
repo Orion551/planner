de013bba174c51a68de89b6a9f870797e9f28402
Orion551 <gravanteumberto@outlook.com>
2024-05-06 18:26:27 +0200

    Improved attachment wizard ui

--- src/Components/widgets/attachment-widget.js
+++ src/Components/widgets/attachment-widget.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { Paper } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 export const AttachmentWidget = ({ attachmentData }) => {
   attachmentData;
   return (
     <>
-      <Paper sx={{ width: '120px', height: '120px' }} variant='outlined'>
-        default variant
+      <Paper
+        sx={{ width: '120px', height: '120px', padding: '3px' }}
+        variant='outlined'
+        square={false}
+      >
+        <Box display='flex' flexDirection='column' justifyContent='space-between' height='100%'>
+          {attachmentData.type === 'document' ? (
+            <Box
+              sx={{
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+              }}
+            >
+              <DescriptionOutlinedIcon sx={{ width: '80px', height: '80px', color: '#39A0C0' }} />
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+              }}
+            >
+              <ImageOutlinedIcon sx={{ width: '80px', height: '80px', color: '#D81E15' }} />
+            </Box>
+          )}
+          <Box
+            display='flex'
+            flexDirection='row'
+            justifyContent='space-between'
+            marginLeft='3px'
+            alignItems='center'
+          >
+            <Typography variant='caption'>{attachmentData.attachmentName}</Typography>
+            <MoreHorizIcon />
+          </Box>
+        </Box>
       </Paper>
     </>
   );
