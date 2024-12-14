bae5a84b135e719cc569eaa01aec8d8fcd8a1353
Orion551 <gravanteumberto@outlook.com>
2024-12-14 15:31:39 +0100

    Added /planner-config/userTags route

--- src/Constants/ApiUrl.js
+++ src/Constants/ApiUrl.js
@@ -1,5 +1,6 @@
 export const ApiUrl = {
   plannerConfig: '/api/v1/planner-config',
+  userTags: '/api/v1/planner-config/userTags',
   activities: '/api/v1/activities',
   projects: '/api/v1/projects',
   activitiesPlanner: '/api/v1/activities-planner',
