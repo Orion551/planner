8bed6a2565e60d6cbd5bc66985960106e90dcbb2
Orion551 <gravanteumberto@outlook.com>
2024-12-28 01:05:51 +0100

    @fix: Adjusted prop exposing; Adjusted ui elements and composition

---
+++ src/Components/Tags/TagInfo.view.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Button from '@mui/material/Button';
+import { TagsPalette } from '@Components/Tags/TagsPalette.view';
+import { Box } from '@mui/material';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import TextField from '@mui/material/TextField';
+
+export const TagInfo = ({ tag, handleDeleteTag, handleEditTagName, inputReference }) => {
+  const { t } = useTranslation();
+  console.log('selected tag', tag);
+
+  return (
+    <Box sx={{ p: 2, flexDirection: 'column' }}>
+      <Box>
+        <TextField
+          name='tagName'
+          value={tag.tagName}
+          onChange={handleEditTagName}
+          ref={inputReference}
+          size='small'
+        />
+      </Box>
+      <Box
+        sx={{
+          marginTop: '5px',
+          marginBottom: '5px',
+        }}
+      >
+        <TagsPalette selectedTag={tag} />
+      </Box>
+
+      <Box
+        sx={{
+          marginTop: '5px',
+        }}
+      >
+        <Button
+          sx={{ textTransform: 'none', width: '100%' }}
+          onClick={handleDeleteTag}
+          color='error'
+          startIcon={<DeleteRoundedIcon />}
+        >
+          {t('tags.actions.delete')}
+        </Button>
+      </Box>
+    </Box>
+  );
+};
