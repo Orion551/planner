663a57263bbf36738e6f6a3d61db49d98eda5d1b
Orion551 <gravanteumberto@outlook.com>
2024-12-29 10:14:38 +0100

    Prop drilled more props from TagsList to TagItem

--- src/Components/Tags/TagItemView.js
+++ src/Components/Tags/TagItemView.js
@@ -3,16 +3,25 @@ import { Box, Typography } from '@mui/material';
 import '@Assets/styles/tag.scss';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import IconButton from '@mui/material/IconButton';
+import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
 
 /**
  * Renders the tag item
  * @param tagName {String} - Display name of the tag
  * @param tagColor {String} - HEX value
+ * @param isEmbedded {Boolean} - If the displayed Tag is embedded in another (Client) component or not. Shows conditionally interaction buttons
+ * @param allowRemove {Boolean} - If true -> X button will be rendered within the Tag. Used to remove the tag from an activity/project
  */
-export const TagItemView = ({ tagName, tagColor }) => {
+export const TagItemView = ({
+  tagName,
+  tagColor,
+  embedded = true,
+  allowRemove = false,
+  infoToggle = () => {},
+  remove = () => {},
+}) => {
   return (
     <>
-      {/* TODO: Improve that */}
       <Box
         sx={{
           display: 'flex',
@@ -35,10 +44,22 @@ export const TagItemView = ({ tagName, tagColor }) => {
           <Typography variant='caption' ml={1} mr={1} noWrap={true} fontWeight={500}>
             {tagName}
           </Typography>
+          {allowRemove && (
+            <IconButton size='small' onClick={(e) => remove(e)}>
+              <ClearRoundedIcon
+                sx={{
+                  width: '12px',
+                  height: '12px',
+                }}
+              />
+            </IconButton>
+          )}
         </Box>
-        <IconButton>
-          <MoreHorizIcon />
-        </IconButton>
+        {!embedded && (
+          <IconButton onClick={(e) => infoToggle(e, { tagName: tagName, tagColor: tagColor })}>
+            <MoreHorizIcon />
+          </IconButton>
+        )}
       </Box>
     </>
   );
--- src/Components/Tags/TagsList.view.js
+++ src/Components/Tags/TagsList.view.js
@@ -6,19 +6,27 @@ import { Box } from '@mui/material';
  * Renders a list of tags
  * @param tags {Array<Tag>} - The tags to render
  * @param tagsPalette {Array<String>} - The palette of colors
- * @param tagSelection {Function} - Function that should be provided by the parent
+ * @param handleTagInfoToggle {Function} - Function that should be provided by the parent
+ * @param isEmbedded
  */
-export const TagsListView = ({ tags, tagsPalette, tagSelection = () => {} }) => {
+export const TagsListView = ({
+  tags,
+  tagsPalette,
+  handleTagInfoToggle = () => {},
+  embedded = true,
+}) => {
   return (
     <>
       <Box sx={{ flex: 'column' }}>
         {tags.map((tag, idx) => (
-          <div key={idx} onClick={(e) => tagSelection(e, tag)}>
-            <TagItemView
-              tagName={tag.tagName}
-              tagColor={tagsPalette.find((tP) => tP.id === tag.tagColorId)?.code}
-            />
-          </div>
+          <TagItemView
+            key={idx}
+            tagName={tag.tagName}
+            tagColor={tagsPalette.find((tP) => tP.id === tag.tagColorId)?.code}
+            embedded={embedded}
+            infoToggle={handleTagInfoToggle}
+            remove={null}
+          />
         ))}
       </Box>
     </>
