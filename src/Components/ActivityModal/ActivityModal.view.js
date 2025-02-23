58cdea856641cdae922db4d95757b9160512fb5c
Orion551 <gravanteumberto@outlook.com>
2025-02-23 23:56:20 +0100

    Started refactoring ActivityModal.view.js

--- src/Components/ActivityModal/ActivityModal.view.js
+++ src/Components/ActivityModal/ActivityModal.view.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  IconButton,
+  CloseIcon,
+  DeleteIcon,
+  // eslint-disable-next-line no-unused-vars
+  TagSelect,
+  ActivityPlanGroup,
+} from './MuiImports';
 import { useGlobalState } from '@Context/GlobalStateContext';
 import { useTranslation } from 'react-i18next';
-import { ActivityPlanGroup } from '@Components/Shared/ActivityPlanGroup';
-import DeleteIcon from '@mui/icons-material/Delete';
 import { Actions } from '@Context/Actions';
+// eslint-disable-next-line no-unused-vars
 import { createActivity, deleteActivity } from '@Context/ActionHandlers/HandleActivity';
-import TextField from '@mui/material/TextField';
 import { findScheduledActivity } from '@Utils/FindScheduledActivity';
-import { TagSelect } from '@Components/Tags/TagSelect.view';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
+import { Form, Formik } from 'formik';
+import { TextInput, SelectField } from '@Components/Shared/Inputs';
+import * as Yup from 'yup';
 
 export const ActivityModalView = () => {
   const { state: appState, dispatch } = useGlobalState();
@@ -63,6 +65,7 @@ export const ActivityModalView = () => {
 
   const handleClose = () => dispatch(Actions.toggleActivityModal(false));
 
+  // eslint-disable-next-line no-unused-vars
   const handleChange = (e) => {
     console.log('event', e);
     setActivityForm((prevForm) => ({
@@ -74,6 +77,7 @@ export const ActivityModalView = () => {
     }));
   };
 
+  // eslint-disable-next-line no-unused-vars
   const handleTagSet = (selectedTag) => {
     setActivityForm((prevForm) => ({
       ...prevForm,
@@ -96,19 +100,9 @@ export const ActivityModalView = () => {
     });
   };
 
-  // eslint-disable-next-line no-unused-vars
-  const handleTagSelection = (tag) => {
-    console.log('selected tag:', tag);
-  };
-
   const handleActivityDelete = async () => {
     // dispatch(Actions.deleteActivity(activityForm.id));
     await deleteActivity(dispatch, appState.activityModal.activityId);
-  };
-
-  // TODO: Data should be validated;
-  const handleActivityCreate = async () => {
-    await createActivity(dispatch, activityForm);
   };
 
   return (
@@ -140,90 +134,88 @@ export const ActivityModalView = () => {
         <CloseIcon />
       </IconButton>
       <DialogContent dividers>
-        <FormControl sx={{ minWidth: 120 }} size='small'>
-          <InputLabel id='project-select-label'>
-            {t('activity_modal.projectField.project')}
-          </InputLabel>
-          <Select
-            labelId='project-select-label'
-            value={activityForm.activity.project}
-            label={t('activity_modal.projectField.project')}
-            onChange={handleChange}
-            name='project'
-          >
-            <MenuItem value={null}>
-              <em>{t('activity_modal.projectField.no_project')}</em>
-            </MenuItem>
-            {appState.projects.map((project) => (
-              <MenuItem key={project.id} value={project.id}>
-                {project.projectName}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        {/* Title */}
-        <TextField
-          name={'title'}
-          required={true}
-          placeholder={t('activity_modal.titleField.what_are_you_gonna_do')}
-          value={activityForm.activity.title}
-          onChange={handleChange}
-          label={t('activity_modal.titleField.title')}
-          size='small'
-          margin='normal'
-          sx={{ width: '100%' }}
-        />
-        <TagSelect
-          tags={activityForm.activity.tag}
-          allowMultiple={false}
-          onTagSelect={handleTagSet}
-        />
-        {/* Description */}
-        <TextField
-          fullWidth
-          multiline
-          rows={4}
-          required={false}
-          label={t('activity_modal.descriptionField.description')}
-          name={'description'}
-          onChange={handleChange}
-          value={activityForm.activity.description}
-        />
-        {/* Activity Plan Btns */}
-        <ActivityPlanGroup
-          isDisabled={appState.activityModal.activityId ? true : false}
-          selectedColumns={activityForm.scheduleColumns || []}
-          onColumnSelection={handleColumnSelection}
-        />
-
-        {/* Estimate */}
-        <TextField
-          required={false}
-          label={t('activity_modal.estimateField.estimate')}
-          onChange={handleChange}
-          value={activityForm.activity.estimate}
-          size='small'
-          margin='normal'
-          name={'estimate'}
-        />
+        <Formik
+          initialValues={{
+            activity: {
+              title: '',
+              project: '',
+              tag: null,
+              description: '',
+              estimate: 0,
+            },
+            scheduleColumns: [],
+          }}
+          validationSchema={Yup.object({
+            title: Yup.string()
+              .max(100, t('validation.errors.must_be_100_chars_or_less'))
+              .required(t('validation.required')),
+            project: Yup.string().oneOf([appState.projects]),
+            description: Yup.string().max(1500, t('validation.errors.must_be_1500_chars_or_less')),
+            estimate: Yup.number().positive().integer(),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 500);
+          }}
+        >
+          {() => (
+            <Form>
+              {/* Title */}
+              <TextInput
+                label='title'
+                name='title'
+                type='text'
+                placeholder={t('activity_modal.titleField.what_are_you_gonna_do')}
+              />
+              {/* Project selection */}
+              <SelectField label={t('activity_modal.projectField.project')} name='project'>
+                <option value=''>{t('activity_modal.projectField.no_project')}</option>
+                {appState.projects.map((project) => (
+                  <option key={project.id} value={project.id}>
+                    {project.projectName}
+                  </option>
+                ))}
+              </SelectField>
+              {/* Textarea */}
+              <TextInput
+                label='description'
+                name='description'
+                type='textarea'
+                placeholder={t('activity_modal.descriptionField.description')}
+              />
+              {/* Activity day planning */}
+              <ActivityPlanGroup
+                isDisabled={appState.activityModal.activityId ? true : false}
+                selectedColumns={activityForm.scheduleColumns || []}
+                onColumnSelection={handleColumnSelection}
+              />
+              {/* Estimate */}
+              <TextInput label='estimate' name='estimate' type='number' placeholder='estimate' />
+            </Form>
+          )}
+        </Formik>
       </DialogContent>
 
       <DialogActions>
-        {appState.activityModal.activityId ? (
-          <Button
-            color='error'
-            variant='outlined'
-            size='small'
-            startIcon={<DeleteIcon />}
-            onClick={handleActivityDelete}
-          >
-            {t('activity_modal.buttons.delete')}
-          </Button>
-        ) : (
-          <Button size='small' autoFocus onClick={handleActivityCreate}>
-            {t('activity_modal.buttons.create')}
-          </Button>
-        )}
+        {
+          appState.activityModal.activityId ? (
+            <Button
+              color='error'
+              variant='outlined'
+              size='small'
+              startIcon={<DeleteIcon />}
+              onClick={handleActivityDelete}
+            >
+              {t('activity_modal.buttons.delete')}
+            </Button>
+          ) : null
+          // TODO: Uncomment this to enable the button. These brackets are required ofc ()
+          // <Button size='small' autoFocus onClick={handleActivityCreate}>
+          //   {t('activity_modal.buttons.create')}
+          // </Button>
+        }
       </DialogActions>
     </Dialog>
   );
---
+++ src/Components/ActivityModal/MuiImports.js
@@ -0,0 +1,12 @@
+export {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  IconButton,
+} from '@mui/material';
+export { default as CloseIcon } from '@mui/icons-material/Close';
+export { default as DeleteIcon } from '@mui/icons-material/Delete';
+export { TagSelect } from '@Components/Tags/TagSelect.view';
+export { ActivityPlanGroup } from '@Components/Shared/ActivityPlanGroup';
