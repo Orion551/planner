0a95f874a2052197ea9fbec6ecb32ade8e3bbffd
Orion551 <gravanteumberto@outlook.com>
2025-03-01 18:23:41 +0100

    Attached ActivityPlanGroup component to Formik useField hook to provide validation features

--- src/Components/Shared/ActivityPlanGroup.js
+++ src/Components/Shared/ActivityPlanGroup.js
@@ -3,28 +3,28 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { useGlobalState } from '@Context/GlobalStateContext';
 import { Box } from '@mui/material';
+import { useField } from 'formik';
 
-export const ActivityPlanGroup = ({ selectedColumns = [], onColumnSelection, isDisabled }) => {
+export const ActivityPlanGroup = ({ name, isDisabled, selectedColumns, onColumnSelection }) => {
   const { state: appState } = useGlobalState();
+  //eslint-disable-next-line no-unused-vars
+  const [field, meta, helpers] = useField(name); // Correctly track Formik state
 
   return (
     <Box sx={{ marginBottom: '8px', marginTop: '16px' }}>
       <ButtonGroup disabled={isDisabled} size='small' color='primary' disableElevation>
         {appState.configData.scheduleColumns.map((column) => (
           <Button
-            variant={
-              selectedColumns.length > 0 && selectedColumns.includes(column.columnId)
-                ? 'contained'
-                : 'outlined'
-            }
-            onClick={() => onColumnSelection(column.columnId)}
             key={column.columnId}
+            variant={selectedColumns.includes(column.columnId) ? 'contained' : 'outlined'}
+            onClick={() => onColumnSelection(column.columnId)}
             sx={{ textTransform: 'none' }}
           >
             {column.columnId.substring(0, 3)}
           </Button>
         ))}
       </ButtonGroup>
+      {meta.error && <div className='error'>{meta.error}</div>}
     </Box>
   );
 };
