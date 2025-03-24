0c1d2cf9004e19ed1ee0a9f70d41bd7f36de9ae7
Orion551 <gravanteumberto@outlook.com>
2025-03-24 20:20:48 +0100

    Added dotenv dependency, created 'local' build process that uses .env localhost file to connect to local backend service

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,30 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const webpack = require('webpack');
 const commonConfig = require('./webpack.common');
+const dotenv = require('dotenv');
+
+/**
+ * Based on the selected environment (`localhost` | `development`), it fetches the correct `.env` file and retrieves the `PLANNER_SERVICE_BASE_URL` to know which services to head to;
+ * @param {String} env - Selected environment
+ * @returns {{'process.env': string}} - Returns `PLANNER_SERVICE_BASE_URL`
+ */
+const loadEnv = (env) => {
+  console.log('loading env: ', env);
+  const envFile = env === 'localhost' ? '.env.localhost' : '.env.development';
+  console.log('envfile: ', envFile);
+  dotenv.config({ path: path.resolve(__dirname, envFile) });
+  console.log('process.env', process.env);
+  return {
+    'process.env': JSON.stringify(process.env),
+  };
+};
+
+// Get the `NODE_ENV` from process.env or default to `development`;
+const env = process.env.NODE_ENV || 'development';
+
+// Load the corresponding `.env` file
+const envConfig = loadEnv(env);
 
 module.exports = merge(commonConfig, {
   mode: 'development',
@@ -18,4 +42,5 @@ module.exports = merge(commonConfig, {
       'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
     },
   },
+  plugins: [new webpack.DefinePlugin(envConfig)],
 });
