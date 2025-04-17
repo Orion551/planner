3faae66af16bae63e0ff6b89f8dd25d07468e0f4
Orion551 <gravanteumberto@outlook.com>
2025-04-17 18:15:17 +0200

    ui adjustments

--- src/Components/ProjectItem/ProjectItemView.js
+++ src/Components/ProjectItem/ProjectItemView.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { StatusView } from '@Utils/StatusView';
 import { StatusViewModes } from '@Constants/StatusViewModes';
-import { CircularProgress } from '@mui/material';
-import { useGlobalState } from '@Context/GlobalStateContext';
-import { getProjectCompletion } from '@Utils/HandleProject';
 
 export const ProjectItemView = ({ project, isSelected, onProjectSelected }) => {
-  const { state } = useGlobalState();
-
   return (
     <>
       <Box
@@ -34,18 +29,11 @@ export const ProjectItemView = ({ project, isSelected, onProjectSelected }) => {
           flexDirection='row'
           sx={{ alignItems: 'center', gap: '10px', userSelect: 'none' }}
         >
-          <CircularProgress
-            sx={{ color: (theme) => theme.palette.success.main }}
-            variant='determinate'
-            value={getProjectCompletion(state.activities, project.projectActivities)}
-            size={15}
-            thickness={6}
-          />
           <Typography
             variant='body2'
             sx={{
               textTransform: 'none',
-              maxWidth: '170px',
+              maxWidth: '180px',
               overflow: 'hidden',
               textOverflow: 'ellipsis',
               whiteSpace: 'nowrap',
