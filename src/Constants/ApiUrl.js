e7213b37bf63dc0202ba933721ad3754ebe90d86
Orion551 <gravanteumberto@outlook.com>
2025-04-22 22:52:04 +0200

    Testing a fix over incorrect usage of ApiUrls

--- src/Constants/ApiUrl.js
+++ src/Constants/ApiUrl.js
@@ -1,10 +1,23 @@
+// export const ApiUrl = {
+//   plannerServiceBaseUrl: process.env.REACT_APP_PLANNER_SERVICE_BASE_URL,
+//   plannerConfig: '/api/v1/planner-config',
+//   userTags: '/api/v1/planner-config/userTags',
+//   activities: '/api/v1/activities',
+//   projects: '/api/v1/projects',
+//   auth: '/api/v1/auth',
+//   activitiesPlanner: '/api/v1/activities-planner',
+//   scheduleColumns: '/scheduleColumns',
+// };
+
+const base = process.env.REACT_APP_PLANNER_SERVICE_BASE_URL;
+
 export const ApiUrl = {
-  plannerServiceBaseUrl: process.env.REACT_APP_PLANNER_SERVICE_BASE_URL,
-  plannerConfig: '/api/v1/planner-config',
-  userTags: '/api/v1/planner-config/userTags',
-  activities: '/api/v1/activities',
-  projects: '/api/v1/projects',
-  auth: '/api/v1/auth',
-  activitiesPlanner: '/api/v1/activities-planner',
-  scheduleColumns: '/scheduleColumns',
+  plannerServiceBaseUrl: base,
+  plannerConfig: `${base}/api/v1/planner-config`,
+  userTags: `${base}/api/v1/planner-config/userTags`,
+  activities: `${base}/api/v1/activities`,
+  projects: `${base}/api/v1/projects`,
+  auth: `${base}/api/v1/auth`,
+  activitiesPlanner: `${base}/api/v1/activities-planner`,
+  scheduleColumns: `${base}/scheduleColumns`,
 };
